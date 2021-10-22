39bb130846d4a43e41c2fd80e5ee11cebaee7f64
Hady Mohammed <spacejupiter0@gmail.com>
2021-10-22 14:54:26 +0200

    Added babel loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,12 @@
 module.exports = {
     module = {
         mode: "development",
-        
+        rules: [
+            {
+                test: "/\.js$/",
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
     }
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,12 @@
 module.exports = {
     module = {
         mode: "production",
-        
+        rules: [
+            {
+                test: "/\.js$/",
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
     }
 }
